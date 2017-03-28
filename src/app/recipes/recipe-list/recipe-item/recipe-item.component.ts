fc11a1f053d099d12befc240fb163a2a8b36a032
Naz <post@nazs.net>
2017-03-28 12:08:04 +0100

    moved recipe items view to its own template

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+   // to bind this from outside (recipe-list.component.html)
+   @Input() recipe = Recipe;
+   constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+   ngOnInit() {
+   }
 
 }
