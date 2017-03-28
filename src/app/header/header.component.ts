c083f4c8ef16a9b0d74515c82ca84dc9d3403acc
Naz <post@nazs.net>
2017-03-28 11:50:44 +0100

    loadedFeature created to navigate recipe and shopping list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+   // to make it listenable from the app.component (parent)
+   @Output() featureSelected = new EventEmitter<string>();
+   onSelect(feature: string) {
+      this.featureSelected.emit(feature);
+   }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
